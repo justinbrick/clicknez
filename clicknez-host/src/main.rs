a3163bc9b1738df820b5ecd0025a0ac7566a1028
Justin <justin@justin.directory>
2025-07-06 22:12:00 +0000

    feat(host): route requests to middleware
    this should allow for routing to the OpenID middleware, which will attach state to all requests as to whether the user is contacting from Entra, Google Workspaces, or some other approved OpenID provider.

--- clicknez-host/src/main.rs
+++ clicknez-host/src/main.rs
@@ -2,6 +2,7 @@ pub mod auth;
 pub mod workflow;
 
 
+#[derive(Clone)]
 pub struct AppState {
     
 }
@@ -9,4 +10,7 @@ pub struct AppState {
 #[tokio::main]
 async fn main() {
     let state = AppState {};
+    let router = axum::Router::new()
+        .layer(axum::middleware::from_fn(auth::authenticate_request))
+        .with_state(state);
 }
