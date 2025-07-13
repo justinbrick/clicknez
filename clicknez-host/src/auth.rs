5efa8970031908c201a77f33897ae5f25cef1d03
Justin <justin@justin.directory>
2025-07-13 14:27:01 -0400

    feat(auth): add struct for identity

--- clicknez-host/src/auth.rs
+++ clicknez-host/src/auth.rs
@@ -1,5 +1,12 @@
 use axum::{extract::Request, http::{HeaderMap, StatusCode}, middleware::Next, response::Response};
 
+
+pub struct Identity {
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+}
+
 pub async fn authenticate_request(
     headers: HeaderMap,
     request: Request,
