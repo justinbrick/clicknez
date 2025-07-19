63333da04ed81821479b620499f51d90d50e21e9
Justin <justin@justin.directory>
2025-07-19 16:01:41 -0400

    chore: add tracing to axum

--- clicknez-host/src/main.rs
+++ clicknez-host/src/main.rs
@@ -1,3 +1,6 @@
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::filter;
+
 pub mod auth;
 pub mod workflow;
 
@@ -9,9 +12,14 @@ pub struct AppState {
 
 #[tokio::main]
 async fn main() {
+    
+    tracing_subscriber::fmt()
+        .with_max_level(filter::LevelFilter::INFO)
+        .init();
+
     let state = AppState {};
     let router = axum::Router::new()
-        .layer(axum::middleware::from_fn(auth::authenticate_request))
+        .layer(TraceLayer::new_for_http())
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
